354b4afc564e068c4fa30437bca90ecdd70f31b2
DangTu169 <danganhtu160901@gmail.com>
2022-11-10 12:11:24 +0700

    buoi11

--- buoi11/ex112/student.php
+++ buoi11/ex112/student.php
@@ -3,9 +3,41 @@
         var $name;
         var $gender;
         var $birthday;
-        var $hometown;
-        function set_student($new_name){
-            $this->name = $new_name;
+        var $address;
+        function setStudentName($value)
+        {
+            $this->name = $value;
+        }
+        function getStudentName()
+        {
+            return $this->name;
+        }
+    
+        function setStudentGender($value)
+        {
+            $this->gender = $value;
+        }
+        function getStudentGender()
+        {
+            return $this->gender;
+        }
+    
+        function setbirthday($value)
+        {
+            $this->birthday = $value;
+        }
+        function getbirthday()
+        {
+            return $this->birthday;
+        }
+    
+        function setStudentAddress($value)
+        {
+            $this->address = $value;
+        }
+        function getStudentAddress()
+        {
+            return $this->address;
         }
     }
 ?>
