f450099df1df5f6da722e30a21194782afe5455b
DangTu169 <danganhtu160901@gmail.com>
2022-11-10 12:05:42 +0700

    buoi11

---
+++ buoi11/ex111/connection.php
@@ -0,0 +1,16 @@
+<?php
+    class connection{
+        var $conn;
+        function __construct(){
+            $severname = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "test_bai10";
+            $this->conn = new mysqli($severname, $username, $password, $dbname);
+            $this -> conn -> set_charset('utf8');
+            if($this->conn->connect_error){
+                die('connect failed: '.$this->conn->connect_error);
+            }
+        }
+    }
+?>
---
+++ buoi11/ex112/student.php
@@ -0,0 +1,11 @@
+<?php
+    class student{
+        var $name;
+        var $gender;
+        var $birthday;
+        var $hometown;
+        function set_student($new_name){
+            $this->name = $new_name;
+        }
+    }
+?>
